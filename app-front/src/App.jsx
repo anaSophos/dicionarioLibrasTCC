e4ff77f7afdb7811d96205f70027bca9f24de6b1
anaSophos <you@example.com>
2024-05-03 21:35:12 -0300

    estrutura do projeto frontend

---
+++ app-front/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Footer from './components/Footer/footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ app-front/src/components/Footer/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+return (
+    <footer className="text-9xl text-red-700">
+        <div className='bg-inherit'>dsfd</div>
+    </footer>
+)
+}
